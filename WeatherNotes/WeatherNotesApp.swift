97804223cd410c6d4f5367bf4537ef176ae83015
dmytrotavlui-netizen <dmytro.tavlui@itrident.agency>
2026-02-27 16:40:23 +0200

    initial setup with coredata persistence

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Dixon on 27.02.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct WeatherNotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
