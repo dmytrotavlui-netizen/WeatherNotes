5b29305e569c441bffd3a590f6ef8a8e1451b04f
dmytrotavlui-netizen <dmytro.tavlui@itrident.agency>
2026-02-27 19:44:43 +0200

    created service for Core Data

---
+++ WeatherNotes/Services/Storage/StorageService.swift
@@ -0,0 +1,72 @@
+//
+//  StorageService.swift
+//  WeatherNotes
+//
+//  Created by Dixon on 27.02.2026.
+//
+
+import CoreData
+
+class StorageService {
+    
+    private let container: NSPersistentContainer
+    private var context: NSManagedObjectContext {
+        container.viewContext
+    }
+    
+    init() {
+        container = NSPersistentContainer(name: "WeatherNotes")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Error with loading Core Data: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func fetchNotes() throws -> [Note] {
+        let request: NSFetchRequest<NoteEntity> = NoteEntity.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \NoteEntity.date, ascending: false)]
+        
+        let entities = try context.fetch(request)
+        
+        return entities.map { entity in
+            Note(
+                id: entity.id ?? UUID(),
+                text: entity.text ?? "",
+                date: entity.date ?? Date(),
+                temperature: entity.temperature,
+                weatherDesc: entity.weatherDesc ?? "",
+                iconCode: entity.iconCode ?? ""
+            )
+        }
+    }
+    
+    func saveNote(text: String, temperature: Double, weatherDesc: String, iconCode: String) throws {
+        let newNote = NoteEntity(context: context)
+        newNote.id = UUID()
+        newNote.date = Date()
+        newNote.text = text
+        newNote.temperature = temperature
+        newNote.weatherDesc = weatherDesc
+        newNote.iconCode = iconCode
+        
+        if context.hasChanges {
+            try context.save()
+        }
+    }
+    
+    func deleteNote(with id: UUID) throws {
+        let request: NSFetchRequest<NoteEntity> = NoteEntity.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+        request.fetchLimit = 1
+        
+        if let entityToDelete = try context.fetch(request).first {
+            context.delete(entityToDelete)
+            
+            if context.hasChanges {
+                try context.save()
+            }
+        }
+    }
+}
